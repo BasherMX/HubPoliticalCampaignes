980f8caa7f753dc1a49997269001af348baec1a0
BasherMX <brayanvazq1608@gmail.com>
2024-03-07 12:06:43 -0600

    cotization pdf download

--- src/app/Principal/ad-stoke/ad-stoke.component.ts
+++ src/app/Principal/ad-stoke/ad-stoke.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { jsPDF } from 'jspdf';
+
+
 
 @Component({
   selector: 'ad-stoke',
@@ -42,4 +45,111 @@ export class AdStokeComponent {
     const datosPaquete = `Cantidad de personas: ${this.selectedCantidadPersonas}, Cantidad de publicaciones: ${this.cantidadPublicaciones}, Precio total: ${this.precioSemanal}`;
     return `https://wa.me/+523346502871?text=${encodeURIComponent(datosPaquete)}`;
   }
+
+
+
+
+  generatePDF() {
+    
+    // Cargar la fuente Arial
+    const url = 'https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.3/fonts/arial.ttf'; // URL de la fuente Arial
+    const fontName = 'Arial';
+    const fontStyle = 'normal';
+    
+    const doc = new jsPDF();
+    
+    // Cargar la fuente Arial
+    doc.addFileToVFS(fontName + '.ttf', url);
+    doc.addFont(fontName + '.ttf', fontName, fontStyle);
+
+    // Agregar imagen al principio del documento
+    const imageUrl = 'https://basherdev-bashermx.vercel.app/assets/logoForPost.png'; // URL de la imagen que deseas agregar
+    const imageWidth = 50; // Ancho de la imagen en el PDF
+    const imageHeight = 50; // Alto de la imagen en el PDF
+    const imageX = 80; // Posición X de la imagen en el PDF
+    const imageY = 5; // Posición Y de la imagen en el PDF
+    doc.addImage(imageUrl, 'JPEG', imageX, imageY, imageWidth, imageHeight);
+
+    // Definir estilos
+    const boldText = { fontWeight: 'bold' };
+    const normalText = {};
+  
+    // Agregar título
+    doc.setFontSize(18);
+    doc.setFont('Arial', 'bold');
+    doc.text('COTIZACION DE ANUNCIOS PARA REDES SOCIALES', 10, 60);
+    doc.text('BASHERDEV', 10, 70);
+  
+    // Agregar fecha
+    const fechaActual = new Date().toLocaleDateString();
+    doc.setFontSize(12);
+    doc.setFont("Arial", 'normal');
+    doc.text(`Fecha: ${fechaActual}`, 10, 75);
+  
+    // Agregar precios
+    doc.setFontSize(19);
+    doc.setFont("Arial", 'bold');
+    doc.text('Descripción:', 10, 90);
+    
+    doc.setFontSize(12);
+    doc.setFont('Arial', 'normal');
+    doc.text(`Cantidad de personas a alcanzar: ${this.selectedCantidadPersonas}`, 10, 100);
+    doc.text(`Cantidad de publicaciones semanales: ${this.cantidadPublicaciones}`, 10, 110);
+    
+    
+    
+    const precios = [
+      { nombre: 'Precio semanal:', valor: this.precioSemanal },
+      { nombre: 'Precio mensual:', valor: this.precioMensual },
+      { nombre: 'Precio por campaña completa:', valor: this.precioCampCompleta }
+    ];
+    
+    const startY = 110;
+    const lineHeight = 10;
+    let currentY = startY;
+    currentY= currentY+20;
+    
+    doc.setFontSize(19);
+    doc.setFont("Arial", 'bold');
+    doc.text('Precio:', 10, currentY);
+    
+    doc.setFontSize(14);
+    currentY= currentY+10;
+    doc.setFont('Arial', 'normal');
+    precios.forEach(precio => {
+      doc.setFont('Arial', 'normal');
+      doc.setFont("a", 'bold');
+      doc.text(precio.nombre, 10, currentY);
+      doc.setFont("a", 'normal');
+      doc.text("$"+precio.valor, 100, currentY);
+      currentY += lineHeight;
+    });
+  
+    // Calcular la posición centrada horizontalmente
+    const text1 = 'Atentamente:';
+    const text2 = 'Brayan Ulises Vazquez Heredia';
+    const textWidth = doc.getTextWidth(text1);
+    const textWidth2 = doc.getTextWidth(text2);
+    const pageWidth = doc.internal.pageSize.width;
+    const centerX = (pageWidth - textWidth) / 2;
+    const centerX2 = (pageWidth - textWidth2) / 2;
+
+    // Agregar nota final centrada horizontalmente
+    doc.setFontSize(10);
+    doc.setFont('Arial', 'normal');
+    doc.text('Atentamente:', centerX, currentY + 10);
+    doc.text('Brayan Ulises Vazquez Heredia', centerX2+5, currentY + 15);
+        
+    doc.setFontSize(8);
+    doc.setFont("a", 'italic');
+    doc.text('*Los Precios Incluyen IVA', 10, currentY + 45);
+    doc.text('*Precios sujetos a cambios sin previo aviso, actualizados al 1 de marzo de 2024', 10, currentY + 50);
+  
+    // Guardar el PDF
+    doc.save('cotizacion.pdf');
+}
+
+  
+
+  
 }
